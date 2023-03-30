f822e5de3ba9b058bb98cb0810e8b6bbf2319a9a
Data Flaw <82406335+Data-Flaw@users.noreply.github.com>
2023-03-30 18:18:25 +0200

    fix arcs when arc angle >= 360°

--- svg5.js
+++ svg5.js
@@ -125,7 +125,7 @@ const regularPolygon = (cx, cy, nbPoints, radius, angle = 0) => {
     polygon(...pts)
 }
 const arc = (cx, cy, w, h, a1, a2) => {
-    if(Math.abs(a1-a2) >= 360) return ellipse(cx, cy, a1, a2)
+    if(Math.abs(a1-a2) >= 360) return ellipse(cx, cy, w, h)
     
     const _a1 = radians(a1)
     const _a2 = radians(a2)
